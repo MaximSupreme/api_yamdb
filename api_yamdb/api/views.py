fcf70747c8ea7dedeacd0838afd2ad24ea9aa014
Maxim Supreme <chernikovmaks2002@gmail.com>
2025-01-30 21:34:20 +0300

    Update serializers and views

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -3,8 +3,10 @@ from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import filters, status, permissions, viewsets
 from rest_framework.response import Response
 from rest_framework.decorators import action
+from rest_framework.exceptions import ValidationError
 
 from .permissions import IsAdminModeratorAuthorOrReadOnly
+from user.permissions import IsAdminOrReadOnly
 import api.serializers as serializers
 import reviews.models as models
 from api.viewsets import ListCreateDeleteViewset
@@ -15,6 +17,20 @@ class TitleViewSet(viewsets.ModelViewSet):
     serializer_class = serializers.TitleSerializer
     filter_backends = (DjangoFilterBackend,)
     filterset_fields = ('category__slug', 'genre__slug', 'name', 'year')
+    permission_classes = (IsAdminOrReadOnly,)
+
+    def update(self, request, *args, **kwargs):
+        if request.method == 'PUT':
+            return Response(status=status.HTTP_405_METHOD_NOT_ALLOWED)
+        partial = kwargs.pop('partial', False)
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data, partial=partial)
+        try:
+            serializer.is_valid(raise_exception=True)
+        except ValidationError:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+        self.perform_update(serializer)
+        return Response(serializer.data)
 
 
 class GenreViewSet(ListCreateDeleteViewset):
@@ -22,19 +38,32 @@ class GenreViewSet(ListCreateDeleteViewset):
     serializer_class = serializers.GenreSerializer
     filter_backends = (filters.SearchFilter,)
     search_fields = ('name',)
+    permission_classes = (IsAdminOrReadOnly,)
 
+    def destroy(self, request, *args, **kwargs):
+        slug = kwargs.get('pk')
+        instance = get_object_or_404(models.Genre, slug=slug)
+        self.perform_destroy(instance)
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
 class CategoryViewSet(ListCreateDeleteViewset):
     queryset = models.Category.objects.all()
     serializer_class = serializers.CategorySerializer
     filter_backends = (filters.SearchFilter,)
     search_fields = ('name',)
+    permission_classes = (IsAdminOrReadOnly,)
+
+    def destroy(self, request, *args, **kwargs):
+        slug = kwargs.get('pk')
+        instance = get_object_or_404(models.Category, slug=slug)
+        self.perform_destroy(instance)
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
 
 class ReviewViewSet(viewsets.ModelViewSet):
     serializer_class = serializers.ReviewSerializer
     permission_classes = (permissions.IsAuthenticatedOrReadOnly,
-                          IsAdminModeratorAuthorOrReadOnly)
+                        IsAdminModeratorAuthorOrReadOnly)
 
     def get_queryset(self):
         title = get_object_or_404(models.Title, id=self.kwargs.get('title_id'))
@@ -52,10 +81,10 @@ class CommentViewSet(viewsets.ModelViewSet):
 
     def get_queryset(self):
         review = get_object_or_404(models.Review,
-                                   id=self.kwargs.get('review_id'))
+                                id=self.kwargs.get('review_id'))
         return review.comments.all()
 
     def perform_create(self, serializer):
         review = get_object_or_404(models.Review,
-                                   id=self.kwargs.get('review_id'))
+                                id=self.kwargs.get('review_id'))
         serializer.save(author=self.request.user, review=review)
